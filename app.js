94f0bafa88fe398415483572e5f34bb79e796d30
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-08-16 15:46:24 +0530

    setup the development mode throught env

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
+
+// the code below just means, that if we press the sign up then this below line will execute.
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the Subscription Tracker API');
 });// simple / forward slash means that we are targeting it into the homepage only.
-app.listen( PORT,()=>{ // here we are not passing the port then body in the code becuse the res.send or app.listen 
+app.listen( PORT,async()=>{ // here we are not passing the port then body in the code becuse the res.send or app.listen 
     // only takes the string as the input so 3000 and console log are already there.
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+
+    await connectToDatabase();
 })
 
 export default app;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,6 +2,21 @@ import { Router} from "express";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.post('/',(req,res)=>res.send('Subscription created'));
+subscriptionRouter.get('/',(req,res)=>res.send('Get all Subscription'));
+
+subscriptionRouter.get('/:id',(req,res)=>res.send('Get Subscription details'));
+
+subscriptionRouter.post('/',(req,res)=>res.send('Create Subscription'));
+
+subscriptionRouter.put('/:id',(req,res)=>res.send('UPDATE Subscription'));
+
+subscriptionRouter.delete('/',(req,res)=>res.send('Subscription DELETED'));
+
+subscriptionRouter.get('/user/:id',(req,res)=>res.send('GET all user Subscriptions'));
+
+subscriptionRouter.put('/:id/cancel',(req,res)=>res.send('CANCEL Subscriptions'));
+
+subscriptionRouter.get('/upcoming-renewals',(req,res)=>res.send('GET UPCOMING RENEWLS'));
+
 
 export default subscriptionRouter;

082c6af36af18c1c2a7b1b8de6314354f15e6452
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-08-24 02:06:45 +0530

    debugging postman error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,8 +3,8 @@ import { signUp,signIn,signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post ('/sign-up', (req,res)=> res.send ({title:'Sign-up'}));
-authRouter.post ('/sign-in', (req,res)=> res.send ({title:'Sign-in'}));
-authRouter.post ('/sign-out', (req,res)=> res.send ({title:'Sign-out'}));
+authRouter.post ('/sign-up', (req,res)=> res.send ({signUp}));
+authRouter.post ('/sign-up',  signIn);
+authRouter.post ('/sign-out', (req,res)=> res.send ({signOut}));
 
 export default authRouter;
